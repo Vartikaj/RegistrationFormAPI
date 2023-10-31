08122890338f835e5199428779f8b8b177a000ac
vartikaj <vartikajohari57@gmail.com>
2023-10-31 17:52:24 +0530

    Changes related to the

--- controllers/registration-controller.js
+++ controllers/registration-controller.js
@@ -2,48 +2,97 @@
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
+const dependencies = require('../dependencies');
 const registrationForm = require('../models/registrationForm-model');
+const uniqueValidator = dependencies.uniqueValidator;
 
 //=============FOR FORM SUBMISSION==============
-exports.postRegistrationData = asyncHandler(async(req, res, next) => {
+exports.postRegistrationData = asyncHandler(async (req, res, next) => {
     try {
-
-        const insertData = await registrationForm.create(req.body)
-            .then(result => {
-                console.log("Insertion successful:", result);
-            })
-            .catch(error => {
-                console.error("Error during insertion:", error);
-            });
-        res.status(200).json({
-            success: true,
-            data: insertData,
+        /**
+         * FIRST CHECK USERNAME OR ADMISSION NO. IS ALREADY EXIST OR NOT
+         */
+        const checkData = await registrationForm.findOne({
+            $or:[
+                {username : req.body.username},
+                {addmissionno : req.body.addmissionno}
+            ]
         });
+        console.log(checkData);
+
+        if (!checkData) {
+            await registrationForm.create(req.body)
+                .then(result => {
+                    res.status(200).json({
+                        success: true,
+                        mesgcode: 1,
+                        mesgtext: 'Insertion successful'
+                    });
+                })
+                .catch(error => {
+                    res.status(200).json({
+                        success: true,
+                        mesgcode: 2,
+                        mesgtext: 'Error during insertion'
+                    });
+                });
+        } else {
+            if (checkData.username == req.body.username) {
+                res.status(200).json({
+                    success: true,
+                    // mesgtext: (checkData) ? `${req.body.username}  username is already present` : `${req.body.addmissionno}  Addmission number is already present`,
+                    mesgtext: 'Username is already present',
+                    mesgcode: 4
+                });
+            }
+
+            if(checkData.addmissionno == req.body.addmissionno) {
+                res.status(200).json({
+                    success: true,
+                    mesgtext: 'Addmission number is already present',
+                    mesgcode: 5
+                });
+            }
+
+        }
+
     } catch (error) {
-        console.error('Error while inserting data : ', error);
-        next(error);
+        res.status(400).json({
+            success: false,
+            mesgcode: 0,
+            mesgtext: 'Something wrong'
+        });
     }
 });
 //==============================================
 
-exports.postUsername = asyncHandler(async(req,res,next) => {
+//LOGIN THE USER INTO THEIR ACCOUNT
+exports.postLoginData = asyncHandler(async(req, res, next) => {
     try{
-        const checkData = await registrationForm.find(req.body.username);
-
-        if(!checkData){
+        console.log(req.body.username);
+        const contentData = await registrationForm.findOne({username : req.body.username});
+        console.log(contentData);
+        if(contentData) {
             res.status(200).json({
-                success: true
+                success : true,
+                mesgcode : 1,
+                mesgtext : 'Login into portal' 
             });
-        }else {
-            res.status(400).json({
-                success: true,
-                data: "Error because of duplicate data ", error,
+        } else {
+            res.status(200).json({
+                success : false,
+                mesgcode : 2,
+                mesgtext : 'Kindly check username' 
             });
         }
-        
-    } catch (error) {
-        console.error('Error while inserting data : ', error);
+    } catch (error){
+        req.status(200).json({
+            sucess: false,
+            mesgcode: error.message,
+            mesgtext: 'Something wrong'
+        });
         next(error);
     }
-})
+});
+//==============================================
 
--- routes/registration-route.js
+++ routes/registration-route.js
@@ -3,12 +3,10 @@ const router = dependencies.router;
 const {
     //HERE WE PASS THE FUNCTION NAME
     postRegistrationData,
-    postUsername,
-
+    postLoginData,
 } = require('../controllers/registration-controller');
 
 router.route('/postRegistrationData').post(postRegistrationData);
-router.route('/postUsername').post(postUsername);
-
+router.route('/postLoginData').post(postLoginData);
 
 module.exports = router;
